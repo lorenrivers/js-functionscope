0e2575789ffda5e9a03bee4692dd623cf8636c3d
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-01-15 11:27:46 +0000

    function scope workshop

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("Hello world!");
+
+const myName = "Loren";
+
+function sayHello() {
+  const surname = "Rivers";
+  console.log("Hello " + myName + " " + surname + "!");
+}
+sayHello();
+
+console.log(surname);
